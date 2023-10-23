3f0e8997ce4831619b86b66668fd40471e9cf79a
Juliana Marques <marquesdjuliana@gmail.com>
2023-10-23 18:05:10 -0300

    create recursive function for palimdrome - fix

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -10,11 +10,3 @@ def is_palindrome_recursive(word, low_index, high_index):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     return False
-
-
-# word = "Ana"
-# low_index = 0
-# high_index = len(word) - 1
-
-# result = is_palindrome_recursive(word, low_index, high_index)
-# print(result)
--- challenges/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    # low_index = word[low_index].replace(" ", "").lower()
-    # high_index = word[high_index].replace(" ", "").lower()
