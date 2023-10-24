fa5c356c2b3aa8e65da7a16485582b851c5800f2
Juliana Marques <marquesdjuliana@gmail.com>
2023-10-24 15:50:14 -0300

    Creates  function that checks duplicate number

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,19 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not nums
+        or not isinstance(nums, list)
+        or not all(isinstance(num, int) and num > 0 for num in nums)
+    ):
+        return False
+
+    nums.sort()
+
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+
+    return False
+
+# nums = [1, -2, 3, 4, -4, 5]
+# result = find_duplicate(nums)
+# print(result)
